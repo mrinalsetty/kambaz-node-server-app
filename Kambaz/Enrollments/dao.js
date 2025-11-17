b25e751a67a78f69755d97d6e52f6114784ad91d
Mrinal Setty <mrinalsetty.s@gmail.com>
2025-11-17 16:04:34 -0500

    update implementation

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -1,22 +1,21 @@
 import { v4 as uuidv4 } from "uuid";
 export default function EnrollmentsDao(db) {
-  let { enrollments } = db;
-  const findAllEnrollments = () => enrollments;
+  const findAllEnrollments = () => db.enrollments;
   const findEnrollmentsForUser = (userId) =>
-    enrollments.filter((e) => e.user === userId);
+    db.enrollments.filter((e) => e.user === userId);
   const enrollUserInCourse = (userId, courseId) => {
-    const existing = enrollments.find(
+    const existing = db.enrollments.find(
       (e) => e.user === userId && e.course === courseId
     );
     if (existing) {
       return existing;
     }
     const newEnrollment = { _id: uuidv4(), user: userId, course: courseId };
-    enrollments = [...enrollments, newEnrollment];
+    db.enrollments = [...db.enrollments, newEnrollment];
     return newEnrollment;
   };
   const unenrollUserFromCourse = (userId, courseId) => {
-    enrollments = enrollments.filter(
+    db.enrollments = db.enrollments.filter(
       (e) => !(e.user === userId && e.course === courseId)
     );
   };
